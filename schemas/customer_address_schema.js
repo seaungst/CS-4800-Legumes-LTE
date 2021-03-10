ebac7e1997492e6efe5a5299411aef44ba63dcf8
Nicole Casartelli <npcasartelli@gmail.com>
2021-03-09 22:50:16 -0800

    Adding CustomerAdd and PaymentInfo Schema

---
+++ schemas/customer_address_schema.js
@@ -0,0 +1,23 @@
+﻿// testing to see if i can pull from the Customer collection
+// building the schema
+const CustomerAddressSchema = new mongoose.Schema({
+    AddressID: { type: Number },
+    CustomerID: { type: Number },
+    Street: { type: String },
+    City: { type: String },
+    State: { type: String },
+    Zip_Code: { type: String },
+    Is_Shipping: { type: Boolean },
+    Is_Billing: { type: Boolean }
+});
+/* instantiating the model from the schema; instead of storing it in a variable,
+ * we will be using the module.exports function in js to make it available to all
+ * the other javascript programs (see code structure above)
+ */
+const Customer = mongoose.model('PaymentInfo', PaymentInfoSchema, 'PaymentInfo');
+
+// executing the query
+console.log("finding payment info");
+PaymentInfo.find({}, function (err, PaymentInfos) {
+    console.log(PaymentInfos);
+});
---
+++ schemas/payment_info_schema.js
@@ -0,0 +1,22 @@
+﻿// testing to see if i can pull from the Customer collection
+// building the schema
+const PaymentInfoSchema = new mongoose.Schema({
+    CustomerID: { type: Number },
+    Zip_Code: { type: String },
+    Card_Num: { type: Number },
+    CVV: { type: Number },
+    Exp_Date: { type: String },
+    Card_Name: { type: String },
+    Billing_Address_ID: { type: Number }
+});
+/* instantiating the model from the schema; instead of storing it in a variable,
+ * we will be using the module.exports function in js to make it available to all
+ * the other javascript programs (see code structure above)
+ */
+const Customer = mongoose.model('PaymentInfo', PaymentInfoSchema, 'PaymentInfo');
+
+// executing the query
+console.log("finding payment info");
+PaymentInfo.find({}, function (err, PaymentInfos) {
+    console.log(PaymentInfos);
+});
