ec38375d5feeaae9e70d016956098e460a690071
ulturac <33331153+ulturac@users.noreply.github.com>
2021-03-15 19:02:54 -0700

    Added Stock with type Number

--- schemas/items_schema.js
+++ schemas/items_schema.js
@@ -10,7 +10,8 @@ const ItemsSchema = new mongoose.Schema({
     IsPerUnit: { type: Boolean },
     Category: { type: String },
     Subcategory: { type: String },
-    Special: { type: String }	
+    Special: { type: String },
+    Stock{ type: Number }
 
 });
 // instantiating the model from the schema; instead of storing it in a variable, we will be using the module.exports function in js to make it available to all the other javascript programs (see code structure above)
