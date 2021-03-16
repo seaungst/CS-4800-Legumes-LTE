83b4d1c9b76b9060bfcacba73960550f2a2ad415
Ryan <ryan.atienza97@gmail.com>
2021-03-15 23:15:25 -0700

    add phone number & rating to store schema

--- schemas/store_schema.js
+++ schemas/store_schema.js
@@ -3,11 +3,13 @@ const mongoose = require("mongoose");
 const StoreSchema = new mongoose.Schema({
     Store_ID: { type: Number },
     Store_Name: { type: String },
-    Store_Info: {type: String},
-    Address_Street: { type: String},
+    Store_Info: {type: String },
+    Address_Street: { type: String },
     Address_City: { type: String },
     Address_State: { type: String },
-    Address_Zipcode: { type: Number }
+    Address_Zipcode: { type: Number },
+    Phone_Number: { type: String },
+    Rating: { type: Number }
 });
 // instantiating the model from the schema; instead of storing it in a variable, we will be using the module.exports function in js to make it available to all the other javascript programs (see code structure above)
 module.exports = mongoose.model('Store', StoreSchema, 'Store');
