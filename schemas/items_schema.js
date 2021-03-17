a6f4821ba123c6a58ce6de194aa334e57c3b96bc
BeanBench <1luis7018@gmail.com>
2021-03-17 02:29:29 -0700

    Added Image_URL to Items Schema

--- schemas/items_schema.js
+++ schemas/items_schema.js
@@ -2,16 +2,15 @@ const mongoose = require("mongoose");
 
 // building the schema
 const ItemsSchema = new mongoose.Schema({
-    Item_ID: { type: Number },
-    Store_ID: { type: Number },
+    ItemID: { type: Number },
     Item_Name: { type: String },
     Brand_Type: {type: Boolean},
     Price: { type: Number },
     IsPerUnit: { type: Boolean },
     Category: { type: String },
     Subcategory: { type: String },
-    Special: { type: String },
-    Stock: { type: Number }
+    Special: { type: String },	
+    Image_URL: { type: String }
 
 });
 // instantiating the model from the schema; instead of storing it in a variable, we will be using the module.exports function in js to make it available to all the other javascript programs (see code structure above)
