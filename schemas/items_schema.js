316d74d149ba6c1f4b176c04eebee28d41f38700
Glitch (chickpea) <none>
2021-03-19 04:20:16 +0000

    added in store descriptions for store view

--- schemas/items_schema.js
+++ schemas/items_schema.js
@@ -2,15 +2,17 @@ const mongoose = require("mongoose");
 
 // building the schema
 const ItemsSchema = new mongoose.Schema({
-    Item_ID: { type: Number },
     Store_ID: { type: Number },
+    Item_ID: { type: Number },
     Item_Name: { type: String },
     Brand_Type: {type: Boolean},
     Price: { type: Number },
     IsPerUnit: { type: Boolean },
     Category: { type: String },
     Subcategory: { type: String },
-    Special: { type: String }	
+    Special: { type: String },
+    Stock: { type: Number },		
+    Image_URL: { type: String }
 
 });
 // instantiating the model from the schema; instead of storing it in a variable, we will be using the module.exports function in js to make it available to all the other javascript programs (see code structure above)
--- schemas/store_schema.js
+++ schemas/store_schema.js
@@ -3,11 +3,16 @@ const mongoose = require("mongoose");
 const StoreSchema = new mongoose.Schema({
     Store_ID: { type: Number },
     Store_Name: { type: String },
-    Store_Info: {type: String},
-    Address_Street: { type: String},
+    Store_Info: {type: String },
+    Address_Street: { type: String },
     Address_City: { type: String },
     Address_State: { type: String },
-    Address_Zipcode: { type: Number }
+    Address_Zipcode: { type: Number },
+    Phone_Number: { type: String },
+    Rating: { type: Number },
+    Splash_Image: { type: String },
+    Owner_Picture: { type: String },
+    Store_History: { type: String }                                    
 });
 // instantiating the model from the schema; instead of storing it in a variable, we will be using the module.exports function in js to make it available to all the other javascript programs (see code structure above)
 module.exports = mongoose.model('Store', StoreSchema, 'Store');
