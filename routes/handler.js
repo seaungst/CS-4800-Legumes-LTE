edbaccdaa0f54af2c0e6d297e3b1d59c411bb573
Ryan <ryan.atienza97@gmail.com>
2021-04-20 16:30:15 -0700

    add new handler route in routes

---
+++ routes/handler.js
@@ -0,0 +1,11 @@
+// routes responsible for reading handler data and sending back appropriate information
+var express = require('express');
+
+var router = express.Router();
+
+// requiring the handler schemas
+var Handler = require('../schemas/handler_schema');
+
+// define API routes here...
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,13 @@ var RegisterRouter = require('./register')
 var LoginRouter = require('./login')
 var CartRouter = require('./cart')
 var UserRouter = require('./user')
+var HandlerRouter = require('./handler')
 
 router.use("/stores", StoreRouter)
 router.use("/register", RegisterRouter)
 router.use("/login", LoginRouter)
 router.use("/cart", CartRouter)
 router.use("/user", UserRouter)
+router.use("/handler", HandlerRouter)
 
 module.exports = router;
