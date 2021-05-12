fad27ef91659705cfe474aae80ce46b4593764e0
Ryan <ryan.atienza97@gmail.com>
2021-05-11 20:59:26 -0700

    change supertest file to stores

--- __tests__/stores_test.js
+++ __tests__/stores_test.js
@@ -6,7 +6,9 @@ it('Gets the /stores endpoint', async done => {
     // Sends GET request to /stores endpoint
     const response = await request.get('/stores')
 
-    expect(response.text).toBeTruthy()
+    // check if response has all 10 stores
+    expect(response.body.length).toBe(10)
+    // check for success
     expect(response.status).toBe(200)
     done()
 })
