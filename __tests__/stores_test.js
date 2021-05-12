168e34b50ce9c782d4e903a5467b0e422f284fa3
Ryan <ryan.atienza97@gmail.com>
2021-05-11 22:21:44 -0700

    add in stores invalid input test

--- __tests__/stores_test.js
+++ __tests__/stores_test.js
@@ -13,29 +13,42 @@ it('Gets the /stores endpoint', async done => {
     done()
 })
 
-// note to self: test for valid and invalid input
 // test for valid input
 it('Grabs stores information from the /stores/detail endpoint', async done => {
     const response = await request.post('/stores/detail')
     .send({
-        store_id: 1
+        store_id: 2
     })
 
     // check if server returned the correct store name
-    expect(response.body.store.Store_Name).toBe("Lentil Mart");
+    expect(response.body.store.Store_Name).toBe("Soybean Supply");
     // check if items are all from Lentil Mart
     var do_items_match = true; // "do items match the store identified by Store_ID"
     var count = 0;  // counting items
     for(var category in response.body.items){
-        for(var item in response.body.items[category]){
-            if(item.Store_ID != 1)
+        count += response.body.items[category].length;
+        for(var item of response.body.items[category]){
+            if(item.Store_ID != 2)
                 do_items_match = false;
-            count += 1;
         }
     }
     expect(do_items_match).toBe(true);
     // check if item count is correct
     expect(count).toBe(20);
+    // check for success
+    expect(response.status).toBe(200)
+    done()
+})
+
+// test for invalid input
+it('invalid input for /stores/detail endpoint', async done => {
+    const response = await request.post('/stores/detail')
+    .send({
+        store_id: 11
+    })
+
+    console.log(response.body);
+
     // check for success
     expect(response.status).toBe(200)
     done()
